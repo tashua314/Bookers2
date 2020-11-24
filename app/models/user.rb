3d11aab3cb1a471ab8ab8eb6cab6f7e60476286d
Yuta Takahashi <m1205123ing@gmail.com>
2020-11-24 23:19:53 +0900

    init commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  attachment :profile_image
+
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'a016386ca797a4e54ac721dce3aed944cda469b6aff5a25105d36b471d9427dcb44d09fc5f25f469938ba441882850665973ed1b3a41e9b7046dc38b05ef1102'
