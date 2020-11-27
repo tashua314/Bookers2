f329e83f61db4720ffe44e70d10b4d207ec9ad68
Yuta Takahashi <m1205123ing@gmail.com>
2020-11-27 22:09:38 +0900

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     @book = Book.new
   end
 
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
   protected
 
   def configure_permitted_parameters
